6d08b5aef915a439f2bb0b5444d8d65cd0cdb298
Jon Tofant <75683804+JonTofant@users.noreply.github.com>
2025-02-23 13:19:47 +0100

    fixed extraction  folder

--- setup_script.sh
+++ setup_script.sh
@@ -13,6 +13,22 @@ conda activate SAR
 # In your terminal (run this every time you activate the environment)
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CONDA_PREFIX/lib/
 
+# Once inside the folder 
+aria2c -x 16 -s 16 -o bigfile.zip "URL_TO_ZIP_FILE"
+
+# Step 2
+#unzip -q bigfile.zip -d extracted_files &
+
+# step 2
+unzip -q bigfile.zip &
+
+pid=$!
+ionice -c 2 -n 0 nice -n -5 wait $pid
+
+# Delete zip
+rm bigfile.zip
+
+
 sudo apt-get parallel -y
 
 # assuming that train_files.txt includes filepaths to all files you want to preprocess
@@ -35,4 +51,3 @@ python main.py \
 
 
 
-# Once inside the folder 
