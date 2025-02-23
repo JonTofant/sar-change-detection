0e25b907488ea3a9d4a1956756a26c05207ce69f
Jon Tofant <75683804+JonTofant@users.noreply.github.com>
2025-02-23 10:23:42 +0100

    added parallel into setup script.sh

--- setup_script.sh
+++ setup_script.sh
@@ -1,14 +1,19 @@
-# git clone 
-git clone https://github.com/JonTofant/sar-change-detection.git
-
-# Anacodna
+# Anaconda
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86.sh
 
 bash ./Miniconda3-latest-Linux-x86.sh
 
+cd sar-change-detection/
+
+#Conda envrionment
+conda env create -f environment.yml
+
+conda activate SAR
+
 # In your terminal (run this every time you activate the environment)
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CONDA_PREFIX/lib/
 
+sudo apt-get parallel -y
 
 # assuming that train_files.txt includes filepaths to all files you want to preprocess
 parallel --halt now,fail=1 -j 45 -I{} python preprocess_tfrecords.py --dataset_stats stats.json --output_dir processed/train/ {} :::: train_files.txt
@@ -16,6 +21,7 @@ parallel --halt now,fail=1 -j 45 -I{} python preprocess_tfrecords.py --dataset_s
 
 # assuming that val_files.txt includes filepaths to all files you want to preprocess
 parallel --halt now,fail=1 -j 45 -I{} python preprocess_tfrecords.py --dataset_stats stats.json --output_dir processed/val/ {} :::: val_files.txt
+
 
 # Train
 python main.py \
@@ -25,4 +31,5 @@ python main.py \
         --epochs 50 \
         --no_mixed_precision \
         --batch_size 32
+
 
